f36dca9f4d77eda3d91e29d4592d0906a62ed73b
Kareem Elaasar <Kareem Elaasar>
2025-02-08 01:02:41 -0800

    multiprocessing changes

--- crawler/worker.py
+++ crawler/worker.py
@@ -1,40 +1,40 @@
 from threading import Thread
-
 from inspect import getsource
 from utils.download import download
 from utils import get_logger
 import scraper
 import time
-from scraper import save_data
-
 
 class Worker(Thread):
     def __init__(self, worker_id, config, frontier):
         self.logger = get_logger(f"Worker-{worker_id}", "Worker")
         self.config = config
         self.frontier = frontier
-        # basic check for requests in scraper
+
+        # Ensure scraper.py does not import disallowed modules
         assert {getsource(scraper).find(req) for req in {"from requests import", "import requests"}} == {-1}, "Do not use requests in scraper.py"
         assert {getsource(scraper).find(req) for req in {"from urllib.request import", "import urllib.request"}} == {-1}, "Do not use urllib.request in scraper.py"
+
         super().__init__(daemon=True)
-        
+
     def run(self):
+        """Worker thread function: fetch, process, and store links."""
         while True:
             tbd_url = self.frontier.get_tbd_url()
             if not tbd_url:
                 self.logger.info("Frontier is empty. Stopping Crawler.")
                 break
+
             resp = download(tbd_url, self.config, self.logger)
-            self.logger.info(
-                f"Downloaded {tbd_url}, status <{resp.status}>, "
-                f"using cache {self.config.cache_server}.")
+            self.logger.info(f"Downloaded {tbd_url}, status <{resp.status}>, using cache {self.config.cache_server}.")
+            
             scraped_urls = scraper.scraper(tbd_url, resp)
             for scraped_url in scraped_urls:
                 self.frontier.add_url(scraped_url)
+            
             self.frontier.mark_url_complete(tbd_url)
+
+            # Ensure politeness by sleeping if needed
             time.sleep(self.config.time_delay)
         
-        # Saving the data that I created in scraper, we can just print the data, but it would be better to save it in a file
-        self.logger.info("Crawling finished. Saving data...")
-        save_data()
-        self.logger.info("Data saved successfully!")
+        self.logger.info("Crawling finished.")
